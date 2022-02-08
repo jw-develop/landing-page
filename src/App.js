7d7b52d3f6aef32c3415b934f57dfdf264d20bc1
James White <jwhite.develop@gmail.com>
2022-02-07 20:34:05 -0500

    add files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <a
+          className="App-link"
+          href="https://faeran.thewhitefam.net"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Faeran!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
